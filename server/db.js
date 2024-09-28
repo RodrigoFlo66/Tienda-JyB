1450ab5b4ad209d6f342656cac73c2c1bb2a7c90
RodrigoFlo666 <rodrigofloresv666@gmail.com>
2024-09-28 14:51:57 -0400

    Actualiza las dependencias de desarrollo y la configuración de la base de datos

--- server/db.js
+++ server/db.js
@@ -2,11 +2,7 @@ const { Pool } = require("pg");
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
